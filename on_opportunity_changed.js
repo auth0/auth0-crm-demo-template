c6411c27ee79531fc92bcf3f88ad87afe5d8d9dc
Glenn Block <glenn.block@gmail.com>
2017-02-28 10:46:40 -0800

    Create on_opportunity_changed.js

---
+++ on_opportunity_changed.js
@@ -0,0 +1,15 @@
+// You can use 900+ modules here. Supported modules: https://tehsis.github.io/webtaskio-canirequire/
+// e.g.:
+
+var request = require('request');
+
+module.exports = function(ctx, cb) {
+  console.log('On opportunity changed:', ctx.body);
+  var opportunity = ctx.body;
+  
+  if (opportunity.size > 1000) {
+    // send e-mail to manager
+  }
+  
+  cb(null, opportunity);
+};
